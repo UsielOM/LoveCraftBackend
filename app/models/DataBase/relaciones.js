b75b0aef7aad1d4fc9f513dd5dfc5ddda46dcde4
UsielOM <usiel31197@outlook.es>
2022-06-28 14:55:06 -0500

    Foreigkeys

---
+++ app/models/DataBase/relaciones.js
@@ -0,0 +1,8 @@
+const Area = require('../Tablas/Area');
+const Estatus = require('../Tablas/Estatus');
+const Interno = require('../Tablas/Interno');
+const Roll = require('../Tablas/Roll.js');
+const Usuarios = require('../Tablas/Usuarios.js');
+module.exports = function() {
+
+}
