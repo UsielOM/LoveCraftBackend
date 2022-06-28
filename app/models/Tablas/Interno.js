a3960f3ed570abce0f49e60e0dd9d3ecf9cebf5e
UsielOM <usiel31197@outlook.es>
2022-06-28 14:19:39 -0500

    Tabla de Interno

---
+++ app/models/Tablas/Interno.js
@@ -0,0 +1,40 @@
+const { Sequelize } = require('sequelize'); //Esta linea va en todos los modelos que hagan 
+const sequelize = require('../DataBase/configDb'); //Esta linea va en todos los modelos que hagan
+
+
+const Interno = sequelize.define('interno', {
+    idInterno: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+
+    },
+    Direccion: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    Foto: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    Contraseña: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull: false
+    },
+    Fech_ingre: {
+        type: Sequelize.DataTypes.STRING(45),
+        allowNull: false
+    },
+    Edad: {
+        type: Sequelize.DataTypes.INTEGER,
+        allowNull: false
+    },
+
+
+
+
+}, {
+    freezeTableName: true, //Esta linea va en todos los modelos que hagan para que respete el nombre
+    timestamps: false, //Esta linea va en todos los modelos para que no les cree los campo de Creacion y actualizacion 
+
+})
