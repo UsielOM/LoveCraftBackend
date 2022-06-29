b17605f1b7720b3322f44a6dfad68b92b886499e
Angeles-Lisset-Castro-Martinez <101274984+Angeles-Lisset-Castro-Martinez@users.noreply.github.com>
2022-06-29 18:30:29 -0500

    Creación tabla internos

---
+++ app/routes/internosroute.js
@@ -0,0 +1,25 @@
+const Interno = require('../models/Tablas/Interno');
+module.exports = function(app, mysql) {
+
+    app.put('/put/interno/:idInterno', (req, res) => {
+        Interno.update({
+            Nombre: req.body.Nombre,
+            Direccion: req.body.Direccion,
+            Edad: req.body.Edad,
+
+        }, {
+            where: { idInterno: req.params.idInterno }
+        }).then(result => {
+            res.send(result);
+        });
+    });
+
+    app.delete('/delete/interno/:idInterno', (req, res) => {
+        Interno.destroy({
+            where: { idInterno: req.params.idInterno }
+        }).then(result => {
+            res.json(result);
+        });
+    })
+
+}
