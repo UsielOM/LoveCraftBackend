82e1e542d6fb7e1ef630be55a78f903f5103bce4
UsielOM <usiel31197@outlook.es>
2022-07-04 12:02:11 -0500

    Metodo que retorno el ultimo id de la tabla  Usuarios

--- app/routes/usuariosRoute.js
+++ app/routes/usuariosRoute.js
@@ -3,5 +3,12 @@ module.exports = function(app, mysql) {
         mysql.postUsuarios(request.body, function(result) {
             response.send(result);
         })
-    })
+    });
+
+    app.get('/get/idmax', (req, res) => {
+        mysql.getMaximoUsers(function(result) {
+            res.send(result)
+        })
+    });
+
 }
